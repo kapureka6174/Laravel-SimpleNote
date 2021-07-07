5f1e6b0afe778a7c49420d7f995d34acd49346a7
kapureka6174 <mitsu.n.0109@gmail.com>
2021-07-07 12:35:51 +0900

    /createへのルーティングを作成

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
